d683197ede9cc1a74eebcdadac0dc2504e49062d
Diego Gonzalez <diegoaglez91@gmail.com>
2024-05-18 23:59:21 -0600

    test

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/milktech-io/auth-middleware/auth"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/public-endpoint", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Hello, this is a public endpoint"})
+	})
+
+	r.GET("/secure-endpoint", auth.AuthMiddleware(), func(c *gin.Context) {
+		user, _ := c.Get("user")
+		c.JSON(http.StatusOK, gin.H{"message": "Hello, this is a secure endpoint", "user": user})
+	})
+
+	r.GET("/admin-endpoint", auth.AuthMiddleware(), auth.RoleMiddleware("admin"), func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Hello, this is an admin endpoint"})
+	})
+
+	r.Run(":8080")
+}
